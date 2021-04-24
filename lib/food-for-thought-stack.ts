265f788c3444b68b1b646d51e4e739788c90931b
Andrew <agdevlabs@gmail.com>
2021-04-24 20:37:58 +0100

    deploys and can query with single resolver

--- lib/food-for-thought-stack.ts
+++ lib/food-for-thought-stack.ts
@@ -1,9 +1,107 @@
-import * as cdk from '@aws-cdk/core';
+import { RemovalPolicy, Construct, Stack, StackProps } from "@aws-cdk/core";
+import {
+  Bucket,
+  BucketAccessControl,
+  BucketEncryption,
+  EventType,
+} from "@aws-cdk/aws-s3";
+import { Function, Runtime, Code } from "@aws-cdk/aws-lambda";
+import { S3EventSource } from "@aws-cdk/aws-lambda-event-sources";
+import { Table, AttributeType, BillingMode } from "@aws-cdk/aws-dynamodb";
+import {
+  GraphqlApi,
+  Schema,
+  DynamoDbDataSource,
+  Resolver,
+  MappingTemplate,
+  PrimaryKey,
+  Assign,
+  AttributeValues,
+} from "@aws-cdk/aws-appsync";
+import {
+  Effect,
+  ManagedPolicy,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+} from "@aws-cdk/aws-iam";
 
-export class FoodForThoughtStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class FoodForThoughtStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    let landingBucket = new Bucket(this, "landing-bucket", {
+      bucketName: `${id}-bucket`.toLowerCase(),
+      blockPublicAccess: {
+        blockPublicAcls: true,
+        blockPublicPolicy: true,
+        ignorePublicAcls: true,
+        restrictPublicBuckets: true,
+      },
+      accessControl: BucketAccessControl.PRIVATE,
+      encryption: BucketEncryption.KMS_MANAGED,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    let foodDatabase = new Table(this, "food-table", {
+      partitionKey: { name: "FOOD_NAME", type: AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    let uploadRole = new Role(this, "upload-role", {
+      assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
+      managedPolicies: [
+        new ManagedPolicy(this, "upload-policy", {
+          statements: [
+            new PolicyStatement({
+              sid: "allowServices",
+              effect: Effect.ALLOW,
+              resources: ["*"],
+              actions: ["kms:*", "s3:*", "lambda:*", "logs:*", "dynamodb:*"],
+            }),
+          ],
+        }),
+      ],
+    });
+
+    let dynamoUpload = new Function(this, `upload-function`, {
+      functionName: `${id}-upload-function`,
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset("./lib/src"),
+      handler: "foodForThoughtUploader.handler",
+      description: "A function that takes our data and loads it into dynamodb",
+      events: [
+        new S3EventSource(landingBucket, {
+          events: [EventType.OBJECT_CREATED],
+        }),
+      ],
+      environment: { targetDyanamo: foodDatabase.tableName },
+      role: uploadRole,
+    });
+
+    let foodAPI = new GraphqlApi(this, "food-graph-api", {
+      name: `${id}-graphql-api`,
+      schema: Schema.fromAsset("./lib/schema.graphql"),
+    });
+    let foodAPISource = new DynamoDbDataSource(this, "food-graph-source", {
+      api: foodAPI,
+      table: foodDatabase,
+      description: "generic food source for GraphQL",
+      name: "Generic_Food",
+    });
+
+    let getGenericResolver = new Resolver(this, "get-generic-resolver", {
+      api: foodAPI,
+      typeName: "Query",
+      fieldName: "getGenericFood",
+      dataSource: foodAPISource,
+      requestMappingTemplate: MappingTemplate.dynamoDbGetItem(
+        "FOOD_NAME",
+        "FOOD_NAME",
+      ),
+      responseMappingTemplate: MappingTemplate.dynamoDbResultItem(),
+    });
   }
 }
