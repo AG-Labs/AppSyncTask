cff60cb78fc004d10ebbca5e960c0a8d22694060
Andrew <agdevlabs@gmail.com>
2021-04-26 21:35:38 +0100

    allow for separate stacks simultaneously

--- bin/food-for-thought.ts
+++ bin/food-for-thought.ts
@@ -6,16 +6,24 @@ import { FoodForThoughtStack } from "../lib/food-for-thought-stack";
 const app = new cdk.App();
 
 let stage = "dev";
+let identifier = "";
+
 if (app.node.tryGetContext("stage") === "prod") {
   stage = "prod";
 }
-
-let foodForThoughtStack = new FoodForThoughtStack(app, "FoodForThought", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
+if (app.node.tryGetContext("id")) {
+  identifier = identifier + "-" + app.node.tryGetContext("id");
+}
+let foodForThoughtStack = new FoodForThoughtStack(
+  app,
+  `FoodForThought${identifier}`,
+  {
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION,
+    },
+    stage,
   },
-  stage,
-});
+);
 
 cdk.Tags.of(foodForThoughtStack).add("project", "food-for-thought");
